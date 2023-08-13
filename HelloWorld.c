69557f5ed8c8e30c7198c212101d4e77880e4029
lesilediaz <ugwulebo_lesile@outlook.com>
2023-08-13 13:17:09 +0100

    My first git commit for c++

---
+++ HelloWorld.c
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CTemplate.c to edit this template
+ */
+
+#include<stdio.h>
+
+int main(void){
+
+    printf("Hello World!!!...");
+}
---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    cout << "Hello world"; 
+
+    return 0;
+}
