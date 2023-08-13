5c0aecdbb696142d07d48a6a30666ad839c29a64
lesilediaz <ugwulebo_lesile@outlook.com>
2023-08-13 14:47:56 +0100

    variable_dec

---
+++ c++/variable_dec.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // declare a varible name account 
+    int account, account_balance, account_deposit;
+
+    //assign a value to a  variable 
+
+    account_balance = 10000;
+    account_deposit = 500;
+    
+
+}
